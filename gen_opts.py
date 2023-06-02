299d8cc82d0658ee7b617f6f09040bb53f8ff903
Taylor O'Brien <toobrien@amazon.com>
2023-06-01 21:40:50 -0700

    refactor

--- gen_opts.py
+++ gen_opts.py
@@ -4,6 +4,25 @@ from sys import argv
 # usage: python gen_opts.py LON23 NYMEX 6000 8000 50
 
 
+def get_opts(
+    opt_id:     str,
+    exchange:   str,
+    lo_strike:  int,
+    hi_strike:  int,
+    increment:  int
+):
+    
+    opts = []
+
+    for type in ["P", "C"]:
+
+        for i in range(lo_strike, hi_strike + increment, increment):
+
+            opts.append(f"{opt_id} {type}{i}.FUT_OPT.{exchange}")
+    
+    return opts
+
+
 if __name__ == "__main__":
 
     opt_id      = argv[1]
@@ -12,8 +31,8 @@ if __name__ == "__main__":
     hi_strike   = int(argv[4])
     increment   = int(argv[5])
 
-    for type in ["P", "C"]:
+    opts = get_opts(opt_id, exchange, lo_strike, hi_strike, increment)
 
-        for i in range(lo_strike, hi_strike + increment, increment):
+    for opt in opts:
 
-            print(f"{opt_id} {type}{i}.FUT_OPT.{exchange}")
+        print(opt)
---
+++ read_opt_trades.py
@@ -0,0 +1,12 @@
+from gen_opts       import get_opts
+from sys            import argv
+from util.parsers   import tas_rec
+from util.rec_tools import get_tas
+
+
+# usage: ...
+
+
+if __name__ == "__main__":
+
+    pass
