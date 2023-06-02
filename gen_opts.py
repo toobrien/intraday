ac331e8b9876b68ec25509a92047f835bb7a2e86
Taylor O'Brien <toobrien@amazon.com>
2023-06-01 20:58:38 -0700

    added intraday file list

---
+++ gen_opts.py
@@ -0,0 +1,18 @@
+from sys import argv
+
+
+# usage: python gen_opt_list.py LON23 NYMEX 5000 10000 50
+
+if __name__ == "__main__":
+
+    opt_id      = argv[1]
+    exchange    = argv[2]
+    lo_strike   = int(argv[3])
+    hi_strike   = int(argv[4])
+    increment   = int(argv[5])
+
+    for type in ["P", "C"]:
+
+        for i in range(lo_strike, hi_strike + increment, increment):
+
+            print(f"{opt_id} {type}{i}.FUT_OPT.{exchange}")
