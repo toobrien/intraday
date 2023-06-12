10d2be625cb068a29906c23ea9e4a8b0ba27b2db
Taylor O'Brien <toobrien@amazon.com>
2023-06-12 13:51:08 -0700

    index skips expired contracts

--- update_indexes.py
+++ update_indexes.py
@@ -1,5 +1,7 @@
+from datetime       import datetime
 from json           import dumps, loads
 from os             import listdir, remove
+from re             import search
 from util.parsers   import bulk_parse_tas, parse_tas_header, tas_rec
 from util.sc_dt     import ts_to_ds
 from time           import time
@@ -10,6 +12,20 @@ SC_ROOT     = CONFIG["sc_root"]
 FMT         = "%Y-%m-%d"
 IDX_ROOT    = CONFIG["idx_root"]
 INDEX_FNS   = listdir(IDX_ROOT)
+CALENDAR    = {
+    1:  "F",
+    2:  "G",
+    3:  "H",
+    4:  "J",
+    5:  "K",
+    6:  "M",
+    7:  "N",
+    8:  "Q",
+    9:  "U",
+    10: "V",
+    11: "X",
+    12: "Z"
+}
 
 
 def update_index(instrument_id: str):
@@ -91,6 +107,32 @@ if __name__ == "__main__":
         if ".scid" in fn
     ]
 
+    skipped     = 0
+    today       = datetime.now()
+    cur_month   = str(CALENDAR[today.month])
+    cur_year    = str(today.year)[-2:]
+    current_fns = []
+    pattern     = "[FGHJKMNQUVXZ]\d{2}"
+
+    # skip expired contracts
+    
+    for fn in fns:
+
+        expiry = search(pattern, fn)[0]
+
+        exp_month   = expiry[0]
+        exp_year    = expiry[1:]
+
+        if  exp_year > cur_year     or \
+            exp_year == cur_year    and exp_month >= cur_month:
+
+            current_fns.append(fn)
+        
+        else:
+
+            skipped += 1
+
+    fns     = current_fns
     num_fns = len(fns)
 
     for i in range(num_fns):
@@ -103,4 +145,5 @@ if __name__ == "__main__":
 
         print(f"{fn:50s}\t{i + 1}/{num_fns}\t{parsed: 10d}\t{time() - t0:0.1f}s")
 
+    print(f"{skipped} skipped")
     print(f"\n{time() - start:0.1f}s elapsed")
