b5a3f39ed04a2e09bbe3d1c33827ffa265f73330
Taylor O'Brien <toobrien@amazon.com>
2023-07-20 22:19:07 -0700

    fixed padding

--- gen_opts.py
+++ gen_opts.py
@@ -1,7 +1,7 @@
 from sys import argv
 
 
-# usage: python gen_opts.py LON23 NYMEX 6000 8000 50
+# usage: python gen_opts.py LON23 NYMEX 6000 8000 50 0
 
 
 def get_opts(
@@ -9,16 +9,17 @@ def get_opts(
     exchange:   str,
     lo_strike:  int,
     hi_strike:  int,
-    increment:  int
+    increment:  int,
+    fill_width: int
 ):
     
     opts = []
 
-    for type in [ "P", "C" ]:
+    for type in [ "C", "P" ]:
 
         for i in range(lo_strike, hi_strike + increment, increment):
 
-            opts.append(f"{opt_id} {type}{i}.FUT_OPT.{exchange}")
+            opts.append(f"{opt_id} {type}{str(i).rjust(fill_width, '0')}.FUT_OPT.{exchange}")
     
     return opts
 
@@ -30,8 +31,9 @@ if __name__ == "__main__":
     lo_strike   = int(argv[3])
     hi_strike   = int(argv[4])
     increment   = int(argv[5])
+    fill_width  = int(argv[6])
 
-    opts = get_opts(opt_id, exchange, lo_strike, hi_strike, increment)
+    opts = get_opts(opt_id, exchange, lo_strike, hi_strike, increment, fill_width)
 
     for opt in opts:
 
