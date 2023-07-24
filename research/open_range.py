94810584464dc5e78e6b697dcbbeb2be54046963
Taylor O'Brien <toobrien@amazon.com>
2023-07-23 20:32:31 -0700

    +

--- research/open_range.py
+++ research/open_range.py
@@ -8,13 +8,13 @@ from    util.contract_settings  import  get_settings
 from    util.rec_tools          import  get_precision
 
 
-# python research/fly_seq.py ZCU23_FUT_CME 11:19:00 17.17:15 2023-03-01 2023-07-14
+# python research/open_range.py ZCU23_FUT_CME 11:15.11:21 17:00.17:16 2023-03-01 2023-07-23
 
 
 if __name__ == "__main__":
 
     contract_id     = argv[1]
-    prev_close      = argv[2]
+    close_range     = argv[2].split(".")
     open_range      = argv[3].split(".")
     start           = f"{argv[4]}T0" if len(argv) > 4 else None
     end             = f"{argv[5]}T0" if len(argv) > 5 else None
@@ -29,8 +29,66 @@ if __name__ == "__main__":
         
         exit()
 
-    idx = get_sessions(bars, open_range[0], open_range[1])
-    
-    for date, bars in idx.items():
+    close_idx   = get_sessions(bars, close_range[0], close_range[1])
+    open_idx    = get_sessions(bars, open_range[0], open_range[1])
+    date_idx    = sorted(list(set(close_idx.keys()).intersection(set(open_idx.keys()))))
+    dt_key      = lambda b: ( b[bar_rec.date], b[bar_rec.time] )
+    x           = []
+    o_          = []
+    h_          = []
+    l_          = []
+    c_          = []
 
-        pass
+
+    for date in date_idx:
+
+        close_bars  = close_idx[date]
+        open_bars   = open_idx[date]
+
+        if len(open_bars) < 2 or not close_bars:
+
+            continue
+
+        prev_close  = close_bars[-1][bar_rec.last]
+        o           = open_bars[0][bar_rec.open]
+        h           = float("-inf")
+        l           = float("inf")
+        c           = open_bars[-1][bar_rec.last]
+
+        for bar in open_bars:
+
+            h = max(bar[bar_rec.high], h)
+            l = min(bar[bar_rec.low], l)
+
+        x.append(date)
+        o_.append(o - prev_close)
+        h_.append(h - prev_close)
+        l_.append(l - prev_close)
+        c_.append(c - prev_close)
+
+    fig = make_subplots(rows = 1, cols = 1)
+
+    fig.update_layout(
+        { 
+            'xaxis': {
+                'rangeslider': { 'visible': False } 
+            }
+        }
+    )
+
+    fig.add_trace(
+        go.Ohlc(
+            {
+                "x":        x,
+                "open":     o_,
+                "high":     h_,
+                "low":      l_,
+                "close":    c_,
+                "name":     "price"
+            }
+        ),
+        row = 1,
+        col = 1
+    )
+
+    fig.show()
