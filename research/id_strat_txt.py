9d9bd5cf06354d0133ba4c1623400656c48d7677
Taylor O'Brien <toobrien@amazon.com>
2023-09-02 00:30:38 -0700

    module

--- research/id_strat_txt.py
+++ research/id_strat_txt.py
@@ -17,30 +17,33 @@ from    util.rec_tools          import  get_precision
 # python research/id_strat_txt.py SPX:1m fly 15:00:00 15:59:00 15:59:00 1 2023-05-01 2023-09-01 EWMA_FIN 1 -1:1 5.0 5.0
 
 
-EWMA_WIN    = 5
+EWMA_WIN = 5
 
 
-def get_ref_strike(cur_price: float, offset: int):
+def get_ref_strike(
+    cur_price:  float, 
+    offset:     int,
+    strike_inc: float
+):
 
     return strike_inc * round(cur_price / strike_inc) + offset * strike_inc
 
 
-if __name__ == "__main__":
+def run(args: list):
 
-    t0              = time()
-    contract_id     = argv[1]
-    strategy        = argv[2]
-    session_start   = argv[3]
-    session_end     = argv[4]
-    expiration      = argv[5]
-    session_inc     = int(argv[6])
-    date_start      = argv[7] if len(argv) > 7 else None
-    date_end        = argv[8] if len(argv) > 8 else None
-    mode            = argv[9]
-    plot            = int(argv[10])
-    offset_rng      = [ int(i) for i in argv[11].split(":") ]
-    strike_inc      = float(argv[12])
-    params          = [ float(i) for i in argv[13:] ]
+    contract_id     = args[1]
+    strategy        = args[2]
+    session_start   = args[3]
+    session_end     = args[4]
+    expiration      = args[5]
+    session_inc     = int(args[6])
+    date_start      = args[7] if len(args) > 7 else None
+    date_end        = args[8] if len(args) > 8 else None
+    mode            = args[9]
+    plot            = int(args[10])
+    offset_rng      = [ int(i) for i in args[11].split(":") ]
+    strike_inc      = float(args[12])
+    params          = [ float(i) for i in args[13:] ]
     bars            = get_bars(contract_id, f"{date_start}T0", f"{date_end}T0")
     _, tick_size    = get_settings(contract_id)
     precision       = get_precision(str(tick_size))
@@ -49,7 +52,7 @@ if __name__ == "__main__":
 
         print("no bars matched")
         
-        exit()
+        return None
 
     idx         = get_sessions(bars, session_start, session_end)
     f_sigmas    = calc_fsigmas(bars, idx, expiration)               # only need to do this once, on the longest session
@@ -88,11 +91,11 @@ if __name__ == "__main__":
 
                 pricer  = fly if strategy == "fly" else iron_fly
                 a       = [
-                        pricer(cur_price = row[0], mid = get_ref_strike(row[1], offset), width = params[0], f_sigma = row[2])
+                        pricer(cur_price = row[0], mid = get_ref_strike(row[1], offset, strike_inc), width = params[0], f_sigma = row[2])
                         for row in curs
                     ]
                 b       = [
-                        pricer(cur_price = row[0], mid = get_ref_strike(row[1], offset), width = params[0], f_sigma = row[2])
+                        pricer(cur_price = row[0], mid = get_ref_strike(row[1], offset, strike_inc), width = params[0], f_sigma = row[2])
                         for row in fins
                     ]
                 diff    = [ a[i] - b[i] for i in range(len(a)) ]
@@ -104,13 +107,13 @@ if __name__ == "__main__":
                 
                 print("invalid strategy")
 
-                exit()
+                return None
 
             if not y:
 
                 print("invalid mode")
 
-                exit()
+                return None
             
             if "PCT_N" in mode:
 
@@ -141,14 +144,12 @@ if __name__ == "__main__":
             for i in range(len(out))
         ]
 
+    '''
     rows = [
         f"{x[i]:10}" + "\t".join([ f"{out[j][i]:0.{precision}f}" for j in range(len(offsets)) ])
         for i in x_rng
     ]
-
-    for row in rows:
-
-        print(row)
+    '''
 
     if plot:
 
@@ -172,5 +173,24 @@ if __name__ == "__main__":
             )
 
         fig.show()
+
+    rows = {
+        x[i] : [ round(out[j][i], precision) for j in range(len(offsets)) ]
+        for i in x_rng
+    }
+
+    return rows
+
+
+if __name__ == "__main__":
+
+    t0   = time()
+    rows = run(argv)
+
+    if rows:
+
+        for ts, vals in rows.items():
+
+            print(f"{ts:10}" + "\t".join([ str(val) for val in vals ]))
 
     print(f"\nfinished in {time() - t0:0.1f}s")
