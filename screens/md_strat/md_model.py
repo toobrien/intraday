4e3c25487f3cb8b03a0cff0fffc1e497f391d8c0
Taylor O'Brien <toobrien@amazon.com>
2023-09-21 23:34:04 -0700

    .

--- screens/md_strat/md_model.py
+++ screens/md_strat/md_model.py
@@ -210,8 +210,9 @@ if __name__ == "__main__":
     for i in range(len(dt_idx)):
 
         print(f"{dt_idx[i]}:\t{'    '.join([ str(val) for val in np.around(vals[i], 2) ])}")
+    '''
 
     print(f"md_model: {time() - t0:0.1f}")
-    '''
+
 
     pass
