6b285d853c8ae9dbe940c9cc086fb10d36bf1792
Taylor O'Brien <toobrien@amazon.com>
2023-09-24 22:23:20 -0700

    server mostly complete

--- screens/md_strat/server.py
+++ screens/md_strat/server.py
@@ -8,6 +8,7 @@ path.append(".")
 
 from md_model   import model
 
+
 #                                   type    sym     res exps     strat  mode   hist_start hist_end   time_idx       offsets inc params
 # python screens/md_strat/server.py FUT     ZC      1m  20231214 fly    FIN    2018-01-01 2024-01-01 now,2023-09-28 -50:51  2   10
 # python screens/md_strat/server.py IND     SPX     1m  -        fly    FIN    2023-06-01 2023-10-01 now,2023-09-24 -25:26  1   5
@@ -28,21 +29,49 @@ from md_model   import model
 # params:           strategy specific -- see util.v_pricing
 
 
+APP     = Flask(__name__, static_folder = "", static_url_path = "")
+CONFIG  = {
+            "host": "localhost",
+            "port": 8080
+        }
+
+APP.config["CACHE_TYPE"] = "null"
+CORS(APP)
+
+
+@APP.route("/config", methods = [ "GET" ])
+def get_config():
+
+    return Response(dumps(CONFIG))
+
+
+@APP.route("/md_strat")
+def index():
+
+    return APP.send_static_file("./md_strat.html")
+
+
 if __name__ == "__main__":
 
-    ul_type     = argv[1]
-    ul_sym      = argv[2]
-    resolution  = argv[3]
-    ul_exps     = argv[4].split(",")
-    strat       = argv[5]
-    mode        = argv[6]
-    hist_start  = argv[7]
-    hist_end    = argv[8]
-    time_idx    = argv[9].split(",")
-    offsets     = [ float(val) for val in argv[10].split(":") ]
-    params      = [ float(val) for val in argv[11:] ]
+    t0 = time()
 
-    model_sym   = ul_sym
+    CONFIG["ul_type"]     = argv[1]
+    CONFIG["ul_sym"]      = argv[2]
+    CONFIG["resolution"]  = argv[3]
+    CONFIG["ul_exps"]     = argv[4].split(",")
+    CONFIG["strat"]       = argv[5]
+    CONFIG["mode"]        = argv[6]
+    CONFIG["hist_start"]  = argv[7]
+    CONFIG["hist_end"]    = argv[8]
+    CONFIG["time_idx"]    = argv[9].split(",")
+    CONFIG["offsets"]     = [ float(val) for val in argv[10].split(":") ]
+    CONFIG["strike_inc"]  = float(argv[11])         
+    CONFIG["params"]      = [ float(val) for val in argv[12:] ]
+
+    ul_sym      = CONFIG["ul_sym"]
+    ul_type     = CONFIG["ul_type"]
+    resolution  = CONFIG["resolution"]
+    model_sym   = CONFIG["ul_sym"]
 
     if ul_type == "IND":
 
@@ -52,3 +81,25 @@ if __name__ == "__main__":
 
         model_sym = f"{ul_sym}-NQTV"
 
+
+    index, rows = model(
+                    model_sym,
+                    CONFIG["strat"],
+                    CONFIG["mode"], 
+                    CONFIG["hist_start"],
+                    CONFIG["hist_end"],
+                    CONFIG["time_idx"],
+                    CONFIG["offsets"],
+                    CONFIG["strike_inc"],
+                    CONFIG["params"]
+                )
+    
+    CONFIG["index"] = index
+    CONFIG["rows"]  = rows
+
+    print(f"server ready: {time() - t0:0.1f}")
+
+    APP.run(
+        host = CONFIG["host"],
+        port = CONFIG["port"]
+    )
