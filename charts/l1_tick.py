2726f54ac16ae88fa566596f64edcf15182f3240
Taylor O'Brien <toobrien@amazon.com>
2024-02-22 22:30:50 -0800

    .

--- charts/l1_tick.py
+++ charts/l1_tick.py
@@ -61,11 +61,11 @@ def bid_ask_trace(it):
 
 def trade_trace(it):
 
-    x = []
-    y = []
-    z = []
-    c = []
-    t = []
+    x   = []
+    y   = []
+    z   = []
+    c   = []
+    t   = []
 
     r0          = next(it)
     prev_idx    = r0[0]
@@ -75,6 +75,8 @@ def trade_trace(it):
     prev_qty    = r0[4]
 
     for row in it:
+
+        print(row)
 
         idx     = row[0]
         ts      = row[1]
@@ -116,8 +118,9 @@ if __name__ == "__main__":
     bounds          = [ arg for arg in argv if "-" in argv ]
     start           = bounds[0] if len(bounds) > 0 else df["ts_event"][0]
     end             = bounds[1] if len(bounds) > 1 else df["ts_event"][-1]
-    x_type          = "ts" if "ts" in argv else "tick"
+    ts_x            = "ts" in argv
     df              = df.filter((df['ts_event'] >= start) & (df['ts_event'] <= end)).with_row_index()
+    fig             = go.Figure()
 
     if df.is_empty():
 
@@ -133,5 +136,28 @@ if __name__ == "__main__":
     ask_x, ask_y, ask_t = bid_ask_trace(asks.iter_rows())
     
     trade_x, trade_y, trade_z, trade_c, trade_t = trade_trace(trades.iter_rows())
+
+    traces = [
+        ( bid_t if ts_x else bid_x, bid_y, bid_t, "bid", "lines", { "marker": { "color": "#0000FF" }, "line": { "shape": "hv" } } ),
+        ( ask_t if ts_x else ask_x, ask_y, ask_t, "ask", "lines", { "marker": { "color": "#0000FF" }, "line": { "shape": "hv" } } ),
+        ( trade_x if ts_x else trade_t, trade_y, trade_z, "trades", "markers", { "marker_size": trade_z, "marker": { "color": trade_c, "sizemode": "area", "sizeref": 2. * max(trade_z) / (40.**2), "size_min": 4 } } )
+    ]
+
+    for trace in traces:
+
+        t = {
+            "x":    trace[0],
+            "y":    trace[1],
+            "text": trace[2],
+            "mode": trace[4]
+        }
+
+        for key, item in trace[5].items():
+
+            t[key] = item
+
+        fig.add_trace(go.Scattergl(t))
+
+    fig.show()
 
     pass
