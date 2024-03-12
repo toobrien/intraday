1d50c950b2c2fca36b303a53b234f65064a5bb04
Taylor O'Brien <toobrien@amazon.com>
2024-03-11 21:42:46 -0700

    .

--- util/dbn_util.py
+++ util/dbn_util.py
@@ -8,21 +8,39 @@ def strptime(
     FMT:        str, 
     tz:         str
 ) -> pl.DataFrame:
-    
-    df = df.with_columns(
-        pl.col(
-            from_col
-        ).map_elements(
-            lambda dt: f"{dt[0:10]}T{dt[10:]}+0000" if " " in dt else dt # hack, fix serialization in dbn.get_csv
-        ).cast(
-            pl.Datetime
-        ).dt.convert_time_zone(
-            tz
-        ).dt.strftime(
-            FMT
-        ).alias(
-            to_col
+
+    if df[from_col].dtype == pl.String:
+
+        df = df.with_columns(
+            pl.col(
+                from_col
+            ).map_elements(
+                lambda dt: f"{dt[0:10]}T{dt[10:]}+0000" if " " in dt else dt # hack, fix serialization in dbn.get_csv
+            ).cast(
+                pl.Datetime
+            ).dt.convert_time_zone(
+                tz
+            ).dt.strftime(
+                FMT
+            ).alias(
+                to_col
+            )
         )
-    )
+    
+    else:
+
+        # datetime
+
+        df = df.with_columns(
+            pl.col(
+                from_col
+            ).dt.convert_time_zone(
+                tz
+            ).dt.strftime(
+                FMT
+            ).alias(
+                to_col
+            )
+        )
 
     return df
