e5d62d4bee29d7cb7a1b3b74d48d496b3eb49057
Taylor O'Brien <toobrien@amazon.com>
2024-04-05 22:00:58 -0700

    .

--- research/sweeps/gen.py
+++ research/sweeps/gen.py
@@ -1,6 +1,5 @@
 from csv                    import reader, writer
 from os.path                import exists
-from random                 import randint
 from sys                    import argv, path
 from time                   import time
 
@@ -20,6 +19,16 @@ from util.sc_dt             import ts_to_ds
 # 6 qty
 # 7 side
 
+COLS = [
+    "timestamp",
+    "start_rec",
+    "end_rec",
+    "ticks",
+    "min_price",
+    "max_price",
+    "qty",
+    "side"
+]
 
 # python research/sweeps/gen.py ESM24_FUT_CME 5
 
@@ -31,19 +40,80 @@ if __name__ == "__main__":
     t0 = time()
 
     contract_id             = argv[1]
-    min_len                 = argv[2]
+    min_len                 = int(argv[2])
     multiplier, tick_size   = get_settings(contract_id)
     precision               = get_precision(str(tick_size))
     fn                      = f"./research/sweeps/store/{contract_id}.csv"
     rows                    = []
+    mode                    = "w"
     i                       = 0
 
     if exists(fn):
 
-        rows    = [ row for row in reader(fn) ]
-        i       = rows[-1][1]
+        mode = "a"
 
-    recs = quick_tas(contract_id, multiplier, None, i, 0)
+        with open(fn, "r") as fd:
+
+            rows    = [ row for row in reader(fd) ]
+            i       = rows[-1][2]
+
+    recs = quick_tas(contract_id, multiplier, None, i)
+
+    groups  = {}
+    out     = []
+
+    for j in range(len(recs)):
+
+        rec     = recs[i]
+        ts      = str(rec[tas_rec.timestamp])[:-3]
+        price   = rec[tas_rec.price]
+        side    = rec[tas_rec.side]
+
+        if ts not in groups:
+
+            groups[ts] = []
+
+        groups[ts].append((i + j, *rec))
+
+    for ts, group in groups.items():
+
+        prices      = [ rec[tas_rec.price] for rec in group ]
+        min_price   = min(prices)
+        max_price   = max(prices)
+        ticks       = int((max_price - min_price) / tick_size)
+
+        if ticks >= min_len:
+
+            side    = 0 if prices[0] > prices[-1] else 1
+            qty     = sum([ rec[tas_rec.qty] for rec in group ])
+            ts_full = ts_to_ds(group[0][tas_rec.timestamp], FMT)
+            i       = group[0][0]
+            j       = group[-1][0]
+
+            out.append(
+                [
+                    ts_full,
+                    i,
+                    j,
+                    ticks,
+                    min_price,
+                    max_price,
+                    qty,
+                    side
+                ]
+            )
+
+    with open(fn, mode) as fd:
+
+        writer_ = writer(fd)
+
+        if mode == "w":
+
+            writer_.writerow(COLS)
+
+        for row in out:
+
+            writer_.writerow(row)
         
     print(f"{len(recs)} recs\t{time() - t0:0.1f}s")
     
