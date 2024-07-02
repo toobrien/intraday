22ca5cf839908cfb093b158a6a7ef364ff8ae010
Taylor O'Brien <toobrien@amazon.com>
2024-07-02 14:50:41 -0700

    .

--- screens/reg/main.js
+++ screens/reg/main.js
@@ -1,12 +1,14 @@
 const { base_client, mdf }  = require("./ibkr/base_client");
 const { format }            = require("date-fns");
-const fs                    = require("node:fs");;
+const fs                    = require("node:fs");
 const CONFIG                = JSON.parse(process.argv[2]);
 const CLIENT                = new base_client(host = CONFIG.host)
-const DATA                  = { ts: [] };
+let   TS                    = null;
+const DATA                  = {};
+const SYM_MAP               = {};
 
 
-// node main.js '{ "conids": [ 568550526, 637533542 ], "tickers": [ "ES", "EMD" ], "interval": 1000, "host": "localhost" }'
+// node main.js '{ "conids": [ 568550526, 637533542 ], "symbols": [ "ES", "EMD" ], "interval": 1000, "host": "localhost" }'
 
 
 function ws_handler(evt) {
@@ -15,9 +17,22 @@ function ws_handler(evt) {
     
     let msg = JSON.parse(evt.data);
 
-    if (msg.topic.includes('smd')) {
+    if (msg.topic.includes('smd+')) {
 
         console.log(JSON.stringify(msg, null, 2));
+
+        let conid = msg.conid;
+
+        if (conid in DATA) {
+
+            let data = DATA[conid];
+
+            msg[mdf.bid] ? data[mdf.bid] = msg[mdf.bid] : null;
+            msg[mdf.ask] ? data[mdf.ask] = msg[mdf.ask] : null;
+
+            data["mid"] = (data[mdf.bid] + data[mdf.ask]) / 2;
+
+        }
 
     }
 
@@ -28,9 +43,15 @@ async function init() {
 
     await CLIENT.set_ws_handlers(msg_handler = ws_handler);
 
-    for (let conid of CONFIG.conids) {
+    for (let i = 0; i < CONFIG.conids.length; i++) {
 
-        DATA[conid] = [];
+        let conid               = CONFIG.conids[i];
+        let sym                 = CONFIG.symbols[i];
+        DATA[conid]             = {};
+        DATA[conid][mdf.bid]    = null;
+        DATA[conid][mdf.ask]    = null;
+        DATA[conid]["mid"]      = null;
+        SYM_MAP[conid]          = sym;
 
         CLIENT.sub_market_data(conid, [ mdf.bid, mdf.ask ]);
 
