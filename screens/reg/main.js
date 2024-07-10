1e819165a17e6fe9c04d5554219d15ab53d74524
Taylor O'Brien <toobrien@amazon.com>
2024-07-10 08:53:37 -0700

    .

--- screens/reg/main.js
+++ screens/reg/main.js
@@ -1,15 +1,14 @@
-const { base_client, mdf }  = require("./ibkr/base_client");
-const { format }            = require("date-fns");
 const fs                    = require("node:fs");
-const CONFIG                = JSON.parse(process.argv[2]);
+const { format }            = require("date-fns");
+const { base_client, mdf }  = require("./ibkr/base_client");
+const CONFIG                = JSON.parse(fs.readFileSync("./config.json"));
 const CLIENT                = new base_client(host = CONFIG.host);
 const DATA                  = {};
-const SYM_MAP               = {};
 const DATE_FMT              = "yyyy-MM-dd";
 const TS_FMT                = `${DATE_FMT}'T'HH:mm:ss`;
 
 
-// node ./screens/reg/main.js '{ "conids": [ 568550526, 637533542 ], "symbols": [ "ES", "EMD" ], "interval": 1000, "host": "localhost", "debug": false }'
+// node ./screens/reg/main.js
 
 
 function ws_handler(evt) {
@@ -46,49 +45,49 @@ async function init() {
 
     await CLIENT.set_ws_handlers(msg_handler = ws_handler);
 
-    for (let i = 0; i < CONFIG.conids.length; i++) {
+    for (let conid of Object.keys(CONFIG.symbols)) {
 
-        let conid               = CONFIG.conids[i];
-        let sym                 = CONFIG.symbols[i];
         DATA[conid]             = {};
         DATA[conid][mdf.bid]    = null;
         DATA[conid][mdf.ask]    = null;
         DATA[conid]["mid"]      = null;
-        SYM_MAP[conid]          = sym;
 
         CLIENT.sub_market_data(conid, [ mdf.bid, mdf.ask ]);
 
     }
+
+    setInterval(write_csv, CONFIG.interval)
 
 }
 
 
 async function write_csv() {
 
-    let entries = Object.entries(SYM_MAP);
+    let conids  = Object.keys(CONFIG.symbols);
+    let syms    = Object.values(CONFIG.symbols);
     let vals    = [];
     let date    = Date.now();
     let day     = format(date, DATE_FMT)
     let ts      = format(Date.now(), TS_FMT);
     let fn      = `./csvs/${day}.csv`;
 
-    if (entries.length == 0)
+    if (syms.length == 0)
 
         return;
 
     if (!fs.existsSync(fn)) {
 
-        let header = `ts,${CONFIG.symbols.join(",")}\n`;
+        let header = `ts,${syms.join(",")}\n`;
         
         fs.writeFileSync(fn, header, 'utf8');
 
     }
 
-    for (let [ conid, _ ] of entries) {
+    for (let conid of conids) {
 
         let data = DATA[conid];
 
-        if (!data.mid) 
+        if (!data.mid)
             
             return;
 
@@ -101,8 +100,5 @@ async function write_csv() {
     fs.appendFileSync(fn, row, "utf8");
 
 }
-
-
-setInterval(write_csv, CONFIG.interval)
 
 init();
